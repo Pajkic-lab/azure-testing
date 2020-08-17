05e0b335ae4b4316bfe76f47076d967916f64ea5
Pajkic-lab <markopajkic@ymail.com>
2020-08-17 13:04:36 +0200

    third attempt

---
+++ client/build/precache-manifest.70c05038c71d7b9a1d3ef8b4f09d5714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768de5842ef4e4a2ca77f14939d9ec3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "796343d9ced70ab40486",
+    "url": "/static/css/main.399311be.chunk.css"
+  },
+  {
+    "revision": "b86212c484d34c2cb33e",
+    "url": "/static/js/2.917e584d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.917e584d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796343d9ced70ab40486",
+    "url": "/static/js/main.50e29db1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  }
+]);
